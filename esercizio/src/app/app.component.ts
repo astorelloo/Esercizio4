eafec68064f6b7b40da50da396d71212d8fdd8b1
er tiruzz <tirino.christian@iisgalvanimi.edu.it>
2024-02-22 13:48:51 +0000

    tiruz

--- esercizio/src/app/app.component.ts
+++ esercizio/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponentComponent } from './header-component/header-component.component';
+import { FooterComponentComponent } from './footer-component/footer-component.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponentComponent],
+  imports: [RouterOutlet, HeaderComponentComponent, FooterComponentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- esercizio/src/app/footer-component/footer-component.component.ts
+++ esercizio/src/app/footer-component/footer-component.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-footer-component',
+  selector: 'footer-component',
   standalone: true,
   imports: [],
   templateUrl: './footer-component.component.html',
