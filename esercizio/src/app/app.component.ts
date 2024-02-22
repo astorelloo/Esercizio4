b635eb8cbecf5ab01768f7b570332565262a2291
er tiruzz <tirino.christian@iisgalvanimi.edu.it>
2024-02-22 12:37:08 +0000

    con bootstrap

--- esercizio/src/app/app.component.ts
+++ esercizio/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponentComponent } from './header-component/header-component.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
